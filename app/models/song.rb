8714acb84abdaa3d5cb085425609dc3e245039bc
Fer Hernandez <fh.1111@gmail.com>
2019-01-14 00:00:35 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,7 @@ class Song < ActiveRecord::Base
   def artist_name
     self.try(:artist).try(:name)
   end
+  
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
