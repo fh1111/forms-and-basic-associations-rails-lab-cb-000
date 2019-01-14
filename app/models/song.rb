bb12ec0e9cb718a0da9368a6176a79257bebda6c
Fer Hernandez <fh.1111@gmail.com>
2019-01-14 00:00:57 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,9 +7,17 @@ class Song < ActiveRecord::Base
   def artist_name
     self.try(:artist).try(:name)
   end
-  
+
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
   end
 end
